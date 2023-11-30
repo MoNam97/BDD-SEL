a80901bc727f85515133c84421119f290e7c7bec
MoNam97 <mohamad0namdar@gmail.com>
2023-11-30 20:43:33 +0330

    add root division method (X) to Calculator

--- untitled/src/main/java/calculator/Calculator.java
+++ untitled/src/main/java/calculator/Calculator.java
@@ -4,4 +4,14 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public double X(int a, int b) throws Exception {
+        if (b == 0) {
+            throw new Exception("divideByZero");
+        } else if ((a < 0 && b > 0) || (b < 0 && a > 0)) {
+            throw new Exception("negativeUnderSqrt");
+        } else {
+            return Math.sqrt(((double) a) / ((double) b));
+        }
+    }
 }
